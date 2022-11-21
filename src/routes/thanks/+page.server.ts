8fa46e542ec4e86ba34c86ddef9155cd83f5f98b
fcrozatier <frederic.crozatier@protonmail.com>
2022-11-21 13:17:16 +0100

    create thanks redirect

---
+++ src/routes/thanks/+page.server.ts
@@ -0,0 +1,18 @@
+import { error } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+import { stripe } from "$lib/stripe";
+
+export const load: PageServerLoad = async ({ url }) => {
+  const payment_intent = url.searchParams.get("payment_intent");
+  const client_secret = url.searchParams.get("payment_intent_client_secret");
+
+  if (!client_secret || !payment_intent) {
+    throw error(400);
+  }
+
+  const paymentIntent = await stripe.paymentIntents.retrieve(payment_intent, {
+    client_secret,
+  });
+
+  return { paymentIntent };
+};
---
+++ src/routes/thanks/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<article class="layout-center">
+  <p>
+    {#if data.paymentIntent.status === "succeeded"}
+      Success !
+    {:else if data.paymentIntent.status === "processing"}
+      Payment processing. We'll update you when payment is received
+    {:else if data.paymentIntent.status === "requires_payment_method"}
+      Payment failed. Please try another payment method
+    {:else}
+      Something went wrong
+    {/if}
+  </p>
+</article>
