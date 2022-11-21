38794efc51cecc9ea1205e20cf63eae3ae11f47f
fcrozatier <frederic.crozatier@protonmail.com>
2022-11-21 15:25:13 +0100

    add webhooks

--- README.md
+++ README.md
@@ -1,18 +1,46 @@
 # Stripe integration demo in a sveltekit app
 
-This is a demo project to help you integrate [Stripe](https://stripe.com/en-fr) to your [Sveltekit](https://kit.svelte.dev/) app:
+This is a demo project to help you integrate [Stripe](https://stripe.com) to your [Sveltekit](https://kit.svelte.dev/) app:
 
 - No wrapper libraries used, just `sveltekit` and `stripe`
-- Accessibility taken into account
+- Validation errors
+- Redirect thanks page
+- Webhooks
+- Accessibility
 
 ## How to use?
 
-Add you secret stripe api key to `PRIVATE_STRIPE_API_KEY` and your public api key to `PUBLIC_STRIPE_API_KEY` in the root `.env` file
+Add you stripe api keys to the `.env` file:
 
 .env
 ```
 PRIVATE_STRIPE_API_KEY="sk_..."
 PUBLIC_STRIPE_API_KEY="Pk_..."
+STRIPE_WEBHOOK_SECRET="whsec_..."
 ```
 
 Then `npm run dev` and you can start testing on `localhost:5173`
+
+## Test Stripe Webhook
+
+1. log in with your Stripe account
+```sh
+stripe login
+```
+
+2. Forward events to your webhook
+```sh
+stripe listen --forward-to localhost:5173/webhook
+```
+
+3. Trigger events with the CLI
+```sh
+stripe trigger payment_intent.succeeded
+```
+
+Example events:
+- payment_intent.succeeded
+- payment_intent.processing
+- payment_intent.payment_failed
+
+See all supported events: `stripe trigger --help`
---
+++ src/routes/webhook/+server.ts
@@ -0,0 +1,45 @@
+import { error, type RequestHandler } from "@sveltejs/kit";
+import { stripe } from "$lib/stripe";
+import { STRIPE_WEBHOOK_SECRET } from "$env/static/private";
+import type Stripe from "stripe";
+
+export const POST: RequestHandler = async function ({ request }) {
+  const signature = request.headers.get("stripe-signature");
+  const payload = await request.text();
+
+  if (!signature) {
+    throw error(400, "no_signature");
+  }
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      payload,
+      signature,
+      STRIPE_WEBHOOK_SECRET
+    );
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded": {
+        const paymentIntent = event.data.object as Stripe.PaymentIntent;
+        console.log(paymentIntent);
+        console.log("payment success");
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      }
+
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      throw error(400, `Webhook Error: ${e.message}`);
+    }
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  return new Response(null, { status: 200 });
+};
